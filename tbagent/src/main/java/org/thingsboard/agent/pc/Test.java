b676f63df86fc1ca50b0b1c6a153ebc8b497bbb5
Shen,Weiwei <Weiwei.Shen@Honeywell.com>
2019-11-14 22:13:49 +0800

    code format

--- tbagent/src/main/java/org/thingsboard/agent/pc/Test.java
+++ tbagent/src/main/java/org/thingsboard/agent/pc/Test.java
@@ -21,91 +21,91 @@ public class Test {
 
 	private static Connector connector;
 
-    private static MessageStatusCallback messageStatusCallback;
+	private static MessageStatusCallback messageStatusCallback;
 
-    public static boolean isBroadcastRegisterd = false;
+	public static boolean isBroadcastRegisterd = false;
 
-    private static MessageCallback messageCallback;
-    
-    public static String DEFAULT_MQTT_SERVER_IP = "tcp://106.12.186.57:1883";
+	private static MessageCallback messageCallback;
 
-    public static String ACCESS_TOKEN = "XL5Uk0z68mtEetht6Dft";
-    
+	public static String DEFAULT_MQTT_SERVER_IP = "tcp://106.12.186.57:1883";
+
+	public static String ACCESS_TOKEN = "XL5Uk0z68mtEetht6Dft";
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		if (connector == null) {
-            ConnectManager connectManager = ConnectManager.getInstance();
-            connector = connectManager.init(ConnectType.MQTT);
-        }
-		
+			ConnectManager connectManager = ConnectManager.getInstance();
+			connector = connectManager.init(ConnectType.MQTT);
+		}
+
 		if (connector != null) {
-            if (!connector.isConnected()) {
-                System.out.println("connect to Mqtt server");
-                messageCallback = new MessageCallback() {
-                    @Override
-                    public void onMessage(String message) {
-                        System.out.println("MessageCallback onMessage:" + message);
-                        try {
+			if (!connector.isConnected()) {
+				System.out.println("connect to Mqtt server");
+				messageCallback = new MessageCallback() {
+					@Override
+					public void onMessage(String message) {
+						System.out.println("MessageCallback onMessage:" + message);
+						try {
 //                            dealMqttMessage(message);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                };
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				};
 
-                ConnectStatusCallback connectStatusCallback = new ConnectStatusCallback() {
-                    @Override
-                    public void onSuccess() {
-                        System.out.println("ConnectStatusCallback onSuccess");
+				ConnectStatusCallback connectStatusCallback = new ConnectStatusCallback() {
+					@Override
+					public void onSuccess() {
+						System.out.println("ConnectStatusCallback onSuccess");
 //                        enrollDevice();
-                    }
+					}
 
-                    @Override
-                    public void onFailure() {
-                        System.out.println("ConnectStatusCallback onFailure");
-                    }
+					@Override
+					public void onFailure() {
+						System.out.println("ConnectStatusCallback onFailure");
+					}
 
-                    @Override
-                    public void connectionLost() {
-                        System.out.println("ConnectStatusCallback connectionLost");
-                        disconnectServer();
-                    }
-                };
+					@Override
+					public void connectionLost() {
+						System.out.println("ConnectStatusCallback connectionLost");
+						disconnectServer();
+					}
+				};
 
-                connector.setCallback(messageCallback);
+				connector.setCallback(messageCallback);
 
-                System.out.println("Connectivity is ok, connect to mqtt server");
-                String clientId = UUID.randomUUID().toString();
-                ConnectMqttOptions connectOptions = new ConnectMqttOptions();
-                connectOptions.setUrl(DEFAULT_MQTT_SERVER_IP);
-                connectOptions.setClientId(clientId);
-                connectOptions.setUserName(ACCESS_TOKEN);
-                connectOptions.setSubscribeTopic("toDevice/" + clientId);
-                connectOptions.setPublishTopic("toCloud/" + clientId);
-                
-                connectOptions.setCleanSession(true);
-                connectOptions.setConnectionTimeout(100);
-                connectOptions.setKeepAliveInterval(200);
-                
-                try {
-                	connector.connect(connectOptions, connectStatusCallback);
-                } catch (Exception e) {
-                	e.printStackTrace();
-                }
-            } else {
-            	System.out.println("already connect to Mqtt server");
+				System.out.println("Connectivity is ok, connect to mqtt server");
+				String clientId = UUID.randomUUID().toString();
+				ConnectMqttOptions connectOptions = new ConnectMqttOptions();
+				connectOptions.setUrl(DEFAULT_MQTT_SERVER_IP);
+				connectOptions.setClientId(clientId);
+				connectOptions.setUserName(ACCESS_TOKEN);
+				connectOptions.setSubscribeTopic("toDevice/" + clientId);
+				connectOptions.setPublishTopic("toCloud/" + clientId);
+
+				connectOptions.setCleanSession(true);
+				connectOptions.setConnectionTimeout(100);
+				connectOptions.setKeepAliveInterval(200);
+
+				try {
+					connector.connect(connectOptions, connectStatusCallback);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else {
+				System.out.println("already connect to Mqtt server");
 //                enrollDevice();
-            }
-        }
+			}
+		}
 	}
-	
+
 	private static void disconnectServer() {
-        if (connector != null) {
-            connector.disconnect();
-            connector = null;
-        }
-    }
+		if (connector != null) {
+			connector.disconnect();
+			connector = null;
+		}
+	}
 
 }
